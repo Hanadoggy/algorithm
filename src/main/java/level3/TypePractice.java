d752858998ad2fa0a58236fc0f5c603d36c446d1
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-11-20 14:58:30 +0900

    11/20 ps

--- src/main/java/level3/TypePractice.java
+++ src/main/java/level3/TypePractice.java
@@ -4,6 +4,54 @@ import java.util.*;
 
 public class TypePractice {
 
+    public int furthestNode(int n, int[][] edges) {
+        Queue<Integer> nextNodes = new LinkedList<>();
+        Node[] nodes = new Node[n + 1];
+        boolean[] visited = new boolean[n + 1];
+        int answer = 0;
+
+        for (int i = 0; i < n + 1; i++) {
+            nodes[i] = new Node();
+        }
+        for (int[] edge : edges) {
+            nodes[edge[0]].addSub(edge[1]);
+            nodes[edge[1]].addSub(edge[0]);
+        }
+
+        visited[1] = true;
+        nextNodes.add(1);
+        while (!nextNodes.isEmpty()) {
+            answer = nextNodes.size();
+            List<Integer> adds = new ArrayList<>();
+
+            while (!nextNodes.isEmpty()) {
+                for (int sub : nodes[nextNodes.poll()].getSubs()) {
+                    if (!visited[sub]) {
+                        visited[sub] = true;
+                        adds.add(sub);
+                    }
+                }
+            }
+            if (adds.isEmpty()) {
+                break;
+            }
+            nextNodes.addAll(adds);
+        }
+        return answer;
+    }
+
+    public class Node {
+        List<Integer> subNodes = new ArrayList<>();
+
+        public void addSub(int n) {
+            subNodes.add(n);
+        }
+
+        public List<Integer> getSubs() {
+            return subNodes;
+        }
+    }
+
     public int network(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
         Queue<Integer> next = new LinkedList<>();
